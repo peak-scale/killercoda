30d284ccaba16f0185bd4d6594b2d01700cac8d8
Oliver Bähler <oliverbaehler@hotmail.com>
2024-06-05 13:19:31 +0200

    development

---
+++ opentofu/getting-started/foreground.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir ~/scenario
+cd ~/scenario
--- opentofu/getting-started/step3/verify.sh
+++ opentofu/getting-started/step3/verify.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd ~/scenario
 
-FILE="hello-example.plan"
+FILE="/root/hello-example.plan"
 if [ ! -f "$FILE" ]; then
   echo "$FILE not does exist." && exit 1
 fi
 
 
-FILE2="hello-example.plan.txt"
+FILE2="/root/hello-example.plan.txt"
 if [ ! -f "$FILE2" ]; then
   echo "$FILE2 not does exist." && exit 1
 fi
 
-actual_output=$(tofu show "hello-example.plan")
-stored_output=$(cat/root/hello-example.plan.txt)
+actual_output=$(tofu show "/root/hello-example.plan")
+stored_output=$(cat /root/hello-example.plan.txt)
 
 diff -w -B <(echo "$actual_output") <(echo "$stored_output")
 if [ $? -ne 0 ]; then
