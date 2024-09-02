78b6e937a518623d4fe84bd998be80cde883d970
Oliver Bähler <oliverbaehler@hotmail.com>
2024-09-02 12:17:25 +0200

    feat(docs): change services
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- opentofu/resources/step2/verify.sh
+++ opentofu/resources/step2/verify.sh
@@ -12,7 +12,7 @@ resource "kubernetes_namespace_v1" "namespace" {
 
 resource "kubernetes_service_account_v1" "serviceaccount" {
   metadata {
-    name = "prod-sa"
+    name = "dev-sa"
     namespace = kubernetes_namespace_v1.namespace.metadata.0.name
   }
 }
