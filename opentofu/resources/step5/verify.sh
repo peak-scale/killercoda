bf18fc3b391c5fcd2b2f10bbac644784cb1ddbd2
Oliver Bähler <oliverbaehler@hotmail.com>
2024-09-03 14:17:29 +0200

    feat(docs): change services
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- opentofu/resources/step5/verify.sh
+++ opentofu/resources/step5/verify.sh
@@ -58,11 +58,6 @@ result=$(hcl2json ~/scenario/pods-foreach.tf | jq '.resource.kubernetes_pod_v1."
   ) and (
     .spec[0].container[0].image == "${each.value.image}"
   )')
-if [ "$result" == "false" ]; then
-  exit 1
-fi
-
-cd ~/scenario
-if ! [ $(tofu state ls | grep "kubernetes_pod_v1.for-workload" | wc -l) -ge 3 ]; then
+if [ "$result" != "true" ]; then
   exit 1
 fi
