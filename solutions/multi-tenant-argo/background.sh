93f92bbd3c3fa0e87c1642cda10880025642c9c1
Oliver Bähler <oliverbaehler@hotmail.com>
2025-01-21 23:00:25 +0100

    feat: use podinfo

--- solutions/multi-tenant-argo/background.sh
+++ solutions/multi-tenant-argo/background.sh
@@ -9,13 +9,6 @@ kubectl kustomize /root/.assets/flux/ | kubectl apply -f -
 
 # Install Distribution
 kubectl kustomize /root/.assets/distro/ | kubectl apply -f -
-while [ "$(kubectl get helmrelease -A -o jsonpath='{range .items[?(@.status.observedGeneration<0)]}{.metadata.namespace}{" "}{.metadata.name}{"\n"}{end}' | wc -l)" -ne 0 ]; do
-  echo "Waiting for all HelmReleases to have observedGeneration >= 0..." >> /etc/peak-scale/setup-log
-  sleep 5
-done
-
-# Apply Stuff
-kubectl kustomize /root/.assets/objects/ | kubectl apply -f -
 
 # Install argo Client
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
@@ -24,5 +17,15 @@ rm argocd-linux-amd64
 
 # Install Fluxcd
 curl -s https://fluxcd.io/install.sh | sudo bash
+
+# Verify Distribution
+while [ "$(kubectl get helmrelease -A -o jsonpath='{range .items[?(@.status.observedGeneration<0)]}{.metadata.namespace}{" "}{.metadata.name}{"\n"}{end}' | wc -l)" -ne 0 ]; do
+  echo "Waiting for all HelmReleases to have observedGeneration >= 0..." >> /etc/peak-scale/setup-log
+  sleep 5
+done
+
+# Apply Objects
+kubectl kustomize /root/.assets/objects/ | kubectl apply -f -
+
 
 touch /tmp/finished
