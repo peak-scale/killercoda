845b46404e1278f99b89aac2ae4ea3960b03e64b
Oliver Bähler <oliverbaehler@hotmail.com>
2025-03-18 09:32:32 +0100

    feat: add trivy as showcase

--- playgrounds/trivy/background.sh
+++ playgrounds/trivy/background.sh
@@ -11,6 +11,9 @@ kubectl kustomize /root/.assets/distro/ | kubectl apply -f -
 # Install Flux
 curl -s https://fluxcd.io/install.sh | sudo bash
 
+# Trivy CLI
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.47.0
+
 # Verify Distribution
 while [ "$(kubectl get helmrelease -A -o jsonpath='{range .items[?(@.status.observedGeneration<0)]}{.metadata.namespace}{" "}{.metadata.name}{"\n"}{end}' | wc -l)" -ne 0 ]; do
   echo "Waiting for all HelmReleases to have observedGeneration >= 0..." >> /etc/peak-scale/setup-log
